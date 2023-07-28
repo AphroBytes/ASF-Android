090c571cb4a976b1599ccff7cafee15a80334a4f
Seva Chernakov <68725282+sevcator@users.noreply.github.com>
2023-07-28 08:26:08 +0800

    download lasest asf release

--- asf-android.sh
+++ asf-android.sh
@@ -1,14 +1,14 @@
 # Information script
-echo ""
+clear
 echo "Welcome to my script. Created by sevcator."
 echo ""
 echo "Original tutorial: https://steamcommunity.com/sharedfiles/filedetails/?id=2570297945"
 echo ""
-echo "You need just drink a tea, i just install ArchiSteamFarm. Please wait and chill."
+echo "Auto-Install ArchiSteamFarm"
 echo ""
 
 # Update pkg
-pkg update -f -y
+pkg update -y -o Dpkg::Options::="--force-confnew"
 
 # Installing packages
 pkg install proot-distro -y
@@ -19,18 +19,21 @@ proot-distro install ubuntu
 # Installing requirements for ArchiSteamFarm
 proot-distro login ubuntu -- bash -c 'apt update -y && apt upgrade -y && apt install libicu70 mono-runtime -y'
 
+# Create a user noroot
+proot-distro login ubuntu -- bash -c 'useradd -m noroot'
+
 # Configure Autostart & motd
-echo "proot-distro login ubuntu --fix-low-ports -- bash -c 'DOTNET_GCHeapHardLimit=1C0000000 && /root/asf/ArchiSteamFarm'" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+echo "proot-distro login ubuntu --user noroot --fix-low-ports -- bash -c 'DOTNET_GCHeapHardLimit=1C0000000 && /root/asf/ArchiSteamFarm'" >> /data/data/com.termux/files/usr/etc/bash.bashrc
 echo "Welcome to Termux!" > /data/data/com.termux/files/usr/etc/motd
 echo " " >> /data/data/com.termux/files/usr/etc/motd
 
 # Joining into directory Ubuntu
-cd /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/ubuntu/root
+cd /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/ubuntu/home/noroot
 
 # Creating a folder for ASF and IPC config
-mkdir asf
+mkdir /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/ubuntu/root/asf
 cd /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/ubuntu/root/asf
-mkdir config
+mkdir /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/ubuntu/root/asf/config
 cd /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/ubuntu/root/asf/config
 curl -L -O https://github.com/sevcator/ASF-Android/raw/main/IPC.config
 cd /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/ubuntu/root/asf
@@ -39,20 +42,31 @@ cd /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/ubuntu/
 arch=$(dpkg --print-architecture)
 
 if [ "$arch" = "arm" ]; then
-  curl -L -O https://github.com/JustArchiNET/ArchiSteamFarm/releases/download/5.4.7.3/ASF-linux-arm.zip
+  curl -s https://api.github.com/repos/JustArchiNET/ArchiSteamFarm/latest \
+  | grep "browser_download_url.*linux-arm.zip" \
+  | cut -d : -f 2,3 \
+  | tr -d \" \
+  | xargs -n 1 curl -L -O
   unzip ASF-linux-arm.zip
   clear
   echo "All right!"
   echo ""
 elif [ "$arch" = "armhf" ]; then
-  curl -L -O https://github.com/JustArchiNET/ArchiSteamFarm/releases/download/5.4.7.3/ASF-linux-arm.zip
+  curl -s https://api.github.com/repos/JustArchiNET/ArchiSteamFarm/latest \
+  | grep "browser_download_url.*linux-arm.zip" \
+  | cut -d : -f 2,3 \
+  | tr -d \" \
+  | xargs -n 1 curl -L -O
   unzip ASF-linux-arm.zip
   clear
   echo "All right!"
   echo ""
 elif [ "$arch" = "arm64" ]; then
-  curl -L -O https://github.com/JustArchiNET/ArchiSteamFarm/releases/download/5.4.7.3/ASF-linux-arm64.zip
-  unzip ASF-linux-arm64.zip
+  curl -s https://api.github.com/repos/JustArchiNET/ArchiSteamFarm/latest \
+  | grep "browser_download_url.*linux-arm64.zip" \
+  | cut -d : -f 2,3 \
+  | tr -d \" \
+  | xargs -n 1 curl -L -O
   clear
   echo "All right!"
   echo ""
@@ -60,5 +74,5 @@ else
   clear
   echo "Unknown architecture. Please install ArchiSteamFarm manually."
   echo ""
-  echo "Your architecture: $arch. Please report this on Github Issues"
+  echo "Your architecture: $arch. Please report this on GitHub Issues"
 fi
