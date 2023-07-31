8bbf19e03a344f7974fef298b35ef3d038cab81e
Seva Chernakov <68725282+sevcator@users.noreply.github.com>
2023-07-31 15:56:20 +0800

    asf android termux klass

--- asf-android.sh
+++ asf-android.sh
@@ -35,12 +35,12 @@ proot-distro login ubuntu -- bash -c 'apt update -y && apt upgrade -y && apt ins
 proot-distro login ubuntu -- bash -c 'useradd -m noroot'
 
 # Configure Autostart & motd
-echo 'if telnet "127.0.0.1" "1337" </dev/null 2>&1 | > grep -q "Connected"; then' >> data/data/com.termux/files/usr/etc/bash.bashrc
-echo '  echo "Server is running. Ignoring next actions..."' >> data/data/com.termux/files/usr/etc/bash.bashrc
-echo '  exit' >> data/data/com.termux/files/usr/etc/bash.bashrc
-echo 'else' >> data/data/com.termux/files/usr/etc/bash.bashrc
-echo '  echo "Server not running!"' >> data/data/com.termux/files/usr/etc/bash.bashrc
-echo 'fi' >> data/data/com.termux/files/usr/etc/bash.bashrc
+echo 'if telnet "127.0.0.1" "1337" </dev/null 2>&1 | > grep -q "Connected"; then' >> /data/data/com.termux/files/usr/etc/bash.bashrc
+echo '  echo "Server is running. Ignoring next actions..."' >> /data/data/com.termux/files/usr/etc/bash.bashrc
+echo '  exit' >> /data/data/com.termux/files/usr/etc/bash.bashrc
+echo 'else' >> /data/data/com.termux/files/usr/etc/bash.bashrc
+echo '  echo "Server not running!"' >> /data/data/com.termux/files/usr/etc/bash.bashrc
+echo 'fi' >> /data/data/com.termux/files/usr/etc/bash.bashrc
 echo "proot-distro login ubuntu --user noroot --fix-low-ports -- bash -c 'export DOTNET_GCHeapHardLimit=1C0000000 && /home/noroot/asf/ArchiSteamFarm'" >> /data/data/com.termux/files/usr/etc/bash.bashrc
 echo "Welcome to Termux!" > /data/data/com.termux/files/usr/etc/motd
 echo " " >> /data/data/com.termux/files/usr/etc/motd
